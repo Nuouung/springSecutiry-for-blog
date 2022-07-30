426998801bc557f1462958c6ad490dce52e092cb
Nuouung <jin6016@gmail.com>
2022-07-30 23:38:21 +0900

    JdbcUserDetailsManager 구현 (데이터베이스에서 회원 가져오기)

--- src/main/java/security/study/configuration/SecurityConfiguration.java
+++ src/main/java/security/study/configuration/SecurityConfiguration.java
@@ -5,20 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import security.study.security.authority.CustomGrantedAuthority;
 import security.study.security.service.InMemoryUserDetailsService;
 import security.study.security.user.CustomUser;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 @Configuration
 public class SecurityConfiguration {
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        CustomUser user1 = new CustomUser("jinseok", "1234", new CustomGrantedAuthority("read"));
-        CustomUser user2 = new CustomUser("roni", "1234", new CustomGrantedAuthority("read"));
-        return new InMemoryUserDetailsService(List.of(user1, user2));
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
